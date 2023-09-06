d455596250896acb099aa30697620dfc2d3976aa
Tousif-Ansary <106613916+tousif67@users.noreply.github.com>
2023-09-06 18:42:38 +0530

    Update adminOrder.controller.js

--- src/controller/adminOrder.controller.js
+++ src/controller/adminOrder.controller.js
@@ -3,63 +3,63 @@ const orderService = require("../Services/order.service.js");
 const getAllOrders = async (req, res) => {
   try {
     const orders = await orderService.getAllOrders();
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const confirmOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
     const orders = await orderService.confirmOrder(orderId);
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const placeOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
     const orders = await orderService.placeOrder(orderId);
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const deliverOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
-    const orders = await orderService.deliverOrder(orderId);
+    return const orders = await orderService.deliverOrder(orderId);
     res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const deleteOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
     const orders = await orderService.deleteOrder(orderId);
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const shipOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
     const orders = await orderService.shipOrder(orderId);
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 const cancelOrders = async (req, res) => {
   const orderId = req.params.orderId;
   try {
     const orders = await orderService.cancelOrder(orderId);
-    res.status(200).send(orders);
+    return res.status(200).send(orders);
   } catch (error) {
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
 module.exports = {
